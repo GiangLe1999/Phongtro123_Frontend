a4efd6dc1feb47afbb32dc403972500efc930e75
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-07-18 20:49:51 +0700

    Update apollo client

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -1,22 +1,29 @@
-import { HttpLink, makeVar } from "@apollo/client";
+import { HttpLink, ApolloLink } from "@apollo/client";
 import {
   registerApolloClient,
   ApolloClient,
   InMemoryCache,
 } from "@apollo/experimental-nextjs-app-support";
+import { setContext } from "@apollo/client/link/context";
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
+  const authLink = setContext((request, { headers }) => ({
+    headers: { ...headers, "Apollo-Require-Preflight": "true" },
+  }));
+
+  const httpLink = new HttpLink({
+    // this needs to be an absolute url, as relative urls cannot be used in SSR
+    uri: process.env.NEXT_PUBLIC_SERVER_URL,
+    // you can disable result caching here if you want to
+    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+    // fetchOptions: { cache: "no-store" },
+    fetchOptions: {
+      mode: "no-cors",
+    },
+  });
+
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: process.env.NEXT_PUBLIC_SERVER_URL,
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
-      fetchOptions: {
-        mode: "no-cors",
-      },
-    }),
+    link: ApolloLink.from([authLink, httpLink]),
   });
 });
