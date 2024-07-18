efac0984d9ad1c5cfbe06a70087e91420a67a503
Lê Thanh Giang <legiangbmt09@gmail.com>
2024-07-18 20:59:22 +0700

    Fix apollo client for csrf errors

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -8,15 +8,16 @@ import { setContext } from "@apollo/client/link/context";
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   const authLink = setContext((request, { headers }) => ({
-    headers: { ...headers, "Apollo-Require-Preflight": "true" },
+    headers: {
+      ...headers,
+      "Apollo-Require-Preflight": "true",
+      "x-apollo-operation-name": request.operationName || "default-operation", // Add this line
+      "content-type": "application/json", // Ensure the content-type is not one of the blocked types
+    },
   }));
 
   const httpLink = new HttpLink({
-    // this needs to be an absolute url, as relative urls cannot be used in SSR
     uri: process.env.NEXT_PUBLIC_SERVER_URL,
-    // you can disable result caching here if you want to
-    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-    // fetchOptions: { cache: "no-store" },
     fetchOptions: {
       mode: "no-cors",
     },
